210b98873336cac5e15c1a456a513dc91461fd6e
Mohamed-Moussa123 <medmoussa3734@gmail.com>
2026-04-13 21:30:27 +0000

    Update database.php

--- vestia_backend-main/vestia_backend-main/vestia_backend/vestia/api/config/database.php
+++ vestia_backend-main/vestia_backend-main/vestia_backend/vestia/api/config/database.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(0);
-ini_set('display_errors', 0);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 define('DB_HOST', getenv('DB_HOST') ?: 'dpg-d7d29777f7vs73enugog-a.frankfurt-postgres.render.com');
 define('DB_PORT', getenv('DB_PORT') ?: '5432');
